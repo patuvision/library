29f17b747b8f6d441d92e8c09e5f0a775aafcf22
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2026-08-29 11:55:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ b = {
     4: "book4",
 }
 
+
 def search(query, data=None):
     if data is None:
         return "insert data"
+    
     for item in data:
         if query in item:
-            return item
-    return None
+            print(item)
+        print("user not found")
 
 def dekarte(a, b):
     c = []
@@ -29,8 +31,12 @@ def dekarte(a, b):
 
     return c
 
+while True:
+    i = input("user ->")
 
-data = dekarte(a, b)
-print(search("amir", data))
+    data = dekarte(a, b)
+    if i == "all":
+        print(data)
+    search(i, data)
 # [('ali', 'book1'), ('ali', 'book2'), ('ali', 'book3'), ('ali', 'book4'), ('amir', 'book1'), ('amir', 'book2'), ('amir', 'book3'), ('amir', 'book4'), ('mmd', 'book1'), ('mmd', 'book2'), ('mmd', 'book3'), ('mmd', 'book4'), ('saeed', 'book1'), ('saeed', 'book2'), ('saeed', 'book3'), ('saeed', 'book4')]
 # ('amir', 'book1'), ('amir', 'book2'), ('amir', 'book3'), ('amir', 'book4')
